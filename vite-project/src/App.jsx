2a1634f567e95b6236d1b3dca1e489b6dd205283
Dhananjay-3270 <shinde3270@gmail.com>
2024-05-18 16:50:18 +0530

    1

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [isclicked, setIsclicked] = useState(false);
+  const [timecount, setTimecount] = useState(0);
+  useEffect(() => {
+    let intervalid;
+    if (isclicked) {
+      intervalid = setInterval(() => {
+        setTimecount((prev) => prev + 1);
+      }, 1000);
+    }
+    return () => {
+      clearInterval(intervalid);
+    };
+  }, [isclicked]);
+  const handleChange = () => {
+    setIsclicked(!isclicked);
+  };
+  const handlereset = () => {
+    setIsclicked(false);
+    setTimecount(0);
+  };
+
+  const format = (timecount)=>{
+let min =Math.floor(timecount/60) ;
+let sec = timecount%60 ;
+return `${min}:${sec<10?"0":""}${sec} `
+
+  }
+  return (
+    <>
+      <h1>Stop Watch</h1>
+      <h4>Time: {format(timecount)}</h4>
+      <button onClick={handleChange}>{isclicked ? "Stop" : "Start"}</button>
+      <button onClick={handlereset}>Reset</button>
+    </>
+  );
+}
+
+export default App;
